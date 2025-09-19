6e170fb411885a60e21c8f30dbd0d4d410c6cacc
tirth123 <tirth@gmail.com>
2025-09-19 22:28:06 +0530

    font size updated

--- app/contact-us/page.tsx
+++ app/contact-us/page.tsx
@@ -7,7 +7,7 @@ const CONTACTS = {
   India: {
     phone: "+91 83206-59236",
     address: "Patel Chambers 108, 395006 Surat, India",
-    map: "https://www.google.com/maps?q=Patel+Chambers+108,+395006+Surat,+India&output=embed",
+    map: "https://www.google.com/maps?q=Patel+Chambers,+108,+Kohinoor+Rd,+opp.+Savani+Estate,+Green+Park+Society,+Varachha,+Surat,+Gujarat+395006&output=embed",
   },
   Germany: {
     phone: "+49 15566-345594",
@@ -19,28 +19,30 @@ const CONTACTS = {
 export default function ContactUsPage() {
   const [country, setCountry] = useState<"India" | "Germany">("India");
   return (
-    <main className="w-full pt-20 pb-16 px-6 md:px-8 flex flex-col items-center bg-[var(--white)]">
+    <main className="w-full pt-25 pb-16 px-5 md:px-8 flex flex-col items-center bg-[var(--white)]">
       <div className="max-w-[1440px] w-full mx-auto">
-        <div className="text-center text-[var(--black)] mb-10">
-          <div className="text-sm  mb-2">• Contact Us</div>
-          <h1 className="text-4xl md:text-5xl font-semibold mb-2">
+        <div className="text-center max-w-[930px] mx-auto mb-10">
+          <span className="text-sm  md:text-base leading-[22px] md:leading-6 font-medium text-[var(--black)] mb-4 block">• Contact Us</span>
+          <h2 className="text-2xl md:text-[48px] font-semibold text-[var(--black)] leading-9 md:leading-16 mb-6">
             Get in touch now
-          </h1>
-          <p className="text-[#575757] text-base md:text-lg max-w-2xl mx-auto">
+          </h2>
+          <p className="text-base md:text-xl text-[var(--gray-text)] leading-6 md:leading-8 max-w-[698px] mx-auto">
             Have questions or need a custom solution? Our team is ready to
             assist you with prompt, professional support.
           </p>
         </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-8 mb-10 text-[var(--black)]">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mb-10 text-[var(--black)]">
           {/* Left: Contact Info */}
-          <section className="bg-gray-100 rounded-2xl p-8 flex flex-col gap-4">
-            <h2 className="text-2xl font-semibold mb-2">We’re Here to Help!</h2>
-            <p className="text-[#575757] mb-4">
+          <section className="bg-gray-100 rounded-2xl p-8 flex flex-col gap-6 md:gap-12 order-2 lg:order-1">
+            <div>
+            <h2 className="text-2xl leading-9 md:text-[32px] md:leading-[44px] font-semibold mb-3">We’re Here to Help!</h2>
+            <p className="text-[#575757] text-sm leading-[22px] md:text-lg md:leading-7 ">
               From inquiries to solutions, we're just a message away.
             </p>
+            </div>
             <div>
-              <div className="font-semibold mb-1">Email</div>
-              <div className="flex items-center gap-2 text-orange-500">
+              <div className="text-xl md:leading-[30px] md:text-2xl leading-9 font-semibold mb-4">Email</div>
+              <div className="flex text-sm leading-[22px] md:text-lg md:leading-7 items-center gap-2 text-orange-500">
                 <Image
                   src="/mail.svg"
                   alt="mail"
@@ -57,34 +59,38 @@ export default function ContactUsPage() {
               </div>
             </div>
             <div>
-              <div className="font-semibold mb-1">Phones</div>
+              <div className="text-xl leading-[30px] md:text-2xl md:leading-9 font-semibold mb-1">Phones</div>
               <div className="grid grid-cols-1 md:grid-cols-2 gap-y-2 gap-x-8">
                 <div>
-                  <div className="text-xs">India</div>
-                  <div className="flex items-center gap-2">
+                  <div className="text-sm leading-[22px] md:text-lg md:leading-7 mb-2">India</div>
+                  <div className="text-sm leading-[22px] md:text-lg md:leading-7 flex items-center gap-2">
                     <FiPhone /> {CONTACTS.India.phone}
                   </div>
                 </div>
                 <div>
-                  <div className="text-xs">Germany</div>
-                  <div className="flex items-center gap-2">
+                  <div className="text-sm leading-[22px] md:text-lg md:leading-7 mb-2">Germany</div>
+                  <div className="text-sm leading-[22px] md:text-lg md:leading-7 flex items-center gap-2">
                     <FiPhone /> {CONTACTS.Germany.phone}
                   </div>
                 </div>
               </div>
             </div>
             <div>
-              <div className="font-semibold mb-1">Address</div>
+              <div className="text-xl leading-[30px] md:text-2xl md:leading-9 font-semibold mb-4">Address</div>
               <div className="grid grid-cols-1 md:grid-cols-2 gap-y-2 gap-x-8">
                 <div>
-                  <div className="flex items-center gap-2">
+                  <div className=" flex items-start gap-2 ">
+                    <div className="py-[6px]">
+
                     <Image
                       src="/location.svg"
                       alt="map"
                       width={20}
                       height={20}
-                    />
-                    <span className="block">
+                      
+                      />
+                      </div>
+                    <span className="block text-sm leading-[22px] md:text-lg md:leading-7">
                       Patel Chambers 108,
                       <br />
                       395006 Surat,
@@ -94,14 +100,16 @@ export default function ContactUsPage() {
                   </div>
                 </div>
                 <div>
-                  <div className="flex items-center gap-2">
+                  <div className="flex items-start gap-2">
+                    <div className="py-[6px]">
                     <Image
                       src="/location.svg"
                       alt="map"
                       width={20}
                       height={20}
-                    />
-                    <span className="block">
+                      />
+                      </div>
+                    <span className="block text-sm leading-[22px] md:text-lg md:leading-7">
                       Heighlofstraße 66,
                       <br />
                       81377 Munich,
@@ -114,12 +122,13 @@ export default function ContactUsPage() {
             </div>
           </section>
           {/* Right: Form */}
-          <section className="bg-gray-100 rounded-2xl p-8 flex flex-col gap-4">
-            <form className="flex flex-col gap-4">
-              <div>
+          <section className="bg-gray-100 rounded-2xl p-8 flex flex-col  order-1 lg:order-2">
+            <form className="flex flex-col gap-6 md:gap-10">
+              <div className="space-y-4 md:space-y-5">
+              <div >
                 <label
                   htmlFor="name"
-                  className="block text-sm font-medium mb-1"
+                  className="block text-sm leading-[22px] md:text-lg md:leading-7 font-medium mb-1"
                 >
                   Your Name
                 </label>
@@ -128,13 +137,13 @@ export default function ContactUsPage() {
                   name="name"
                   type="text"
                   placeholder="Your Name"
-                  className="w-full border border-[#c8c8c8] rounded-md px-3 py-2 outline-none focus:ring-2 focus:ring-orange-400 bg-white"
+                  className="w-full text-sm leading-[22px] md:text-lg md:leading-7 border border-[#c8c8c8] rounded-md px-5 py-[14px] outline-none focus:ring-2 focus:ring-orange-400 bg-white"
                 />
               </div>
               <div>
                 <label
                   htmlFor="company"
-                  className="block text-sm font-medium mb-1"
+                  className="block text-sm leading-[22px] md:text-lg md:leading-7 font-medium mb-1"
                 >
                   Company Name
                 </label>
@@ -143,14 +152,14 @@ export default function ContactUsPage() {
                   name="company"
                   type="text"
                   placeholder="Company Name"
-                  className="w-full border border-[#c8c8c8] rounded-md px-3 py-2 outline-none focus:ring-2 focus:ring-orange-400 bg-white"
+                  className="w-full border text-sm leading-[22px] md:text-lg md:leading-7 border-[#c8c8c8] rounded-md px-5 py-[14px] outline-none focus:ring-2 focus:ring-orange-400 bg-white"
                 />
               </div>
-              <div className="flex gap-4">
-                <div className="flex-1">
+              <div className="md:flex gap-4 ">
+                <div className="flex-1 mb-4 md:mb-0">
                   <label
                     htmlFor="email"
-                    className="block text-sm font-medium mb-1"
+                    className="block text-sm leading-[22px] md:text-lg md:leading-7 font-medium mb-1"
                   >
                     Email
                   </label>
@@ -159,13 +168,13 @@ export default function ContactUsPage() {
                     name="email"
                     type="email"
                     placeholder="Email"
-                    className="w-full border border-[#c8c8c8] rounded-md px-3 py-2 outline-none focus:ring-2 focus:ring-orange-400 bg-white"
+                    className="w-full border text-sm leading-[22px] md:text-lg md:leading-7 border-[#c8c8c8] rounded-md px-5 py-[14px] outline-none focus:ring-2 focus:ring-orange-400 bg-white"
                   />
                 </div>
                 <div className="flex-1">
                   <label
                     htmlFor="mobile"
-                    className="block text-sm font-medium mb-1"
+                    className="block text-sm leading-[22px] md:text-lg md:leading-7 font-medium mb-1"
                   >
                     Mobile No.
                   </label>
@@ -174,14 +183,14 @@ export default function ContactUsPage() {
                     name="mobile"
                     type="tel"
                     placeholder="Mobile No."
-                    className="w-full border border-[#c8c8c8] rounded-md px-3 py-2 outline-none focus:ring-2 focus:ring-orange-400 bg-white"
+                    className="w-full border text-sm leading-[22px] md:text-lg md:leading-7 border-[#c8c8c8] rounded-md px-5 py-[14px] outline-none focus:ring-2 focus:ring-orange-400 bg-white"
                   />
                 </div>
               </div>
               <div>
                 <label
                   htmlFor="message"
-                  className="block text-sm font-medium mb-1"
+                  className="block text-sm leading-[22px] md:text-lg md:leading-7 font-medium mb-1"
                 >
                   Message
                 </label>
@@ -190,41 +199,45 @@ export default function ContactUsPage() {
                   name="message"
                   placeholder="What would you like to know about"
                   rows={3}
-                  className="w-full border border-[#c8c8c8] rounded-md px-3 py-2 outline-none focus:ring-2 focus:ring-orange-400 bg-white"
+                  className="w-full border text-sm leading-[22px] md:text-lg md:leading-7 border-[#c8c8c8] rounded-md px-5 py-[14px] outline-none focus:ring-2 focus:ring-orange-400 bg-white"
                 />
               </div>
-              <div className="text-xs text-gray-500 mb-2">
+              <div className="text-sm leading-[22px] md:text-base md:leading-6 text-gray-500 mb-2">
                 <span className="text-orange-500 mr-1">*</span>We promise not to
                 disclose your personal information to third parties.
               </div>
+              </div>
+              <div>
+
               <button
                 type="submit"
-                className="bg-orange-500 text-white rounded-md px-6 py-2 font-semibold flex items-center gap-2 hover:bg-orange-600 transition-all w-fit"
-              >
+                className="bg-[var(--orange)] text-sm leading-[22px] md:text-base md:leading-6 text-white rounded-full px-6 py-[10px] font-medium flex items-center gap-2 hover:bg-orange-600 transition-all w-fit"
+                >
                 Send Message <span className="ml-1">↗</span>
               </button>
+                </div>
             </form>
           </section>
         </div>
         {/* Map Section */}
         <div className="flex flex-col items-center gap-4 mt-8">
-          <div className="flex gap-2 mb-4">
+          <div className="flex bg-[var(--secondary-bg)] rounded-2xl p-1 gap-1 mb-4">
             <button
               onClick={() => setCountry("India")}
-              className={`px-5 py-1 rounded-full font-medium border ${
+              className={`px-5 py-1 rounded-full font-medium  ${
                 country === "India"
                   ? "bg-black text-white"
-                  : "bg-white text-black border-gray-300"
+                  : " text-black "
               }`}
             >
               India
             </button>
             <button
               onClick={() => setCountry("Germany")}
-              className={`px-5 py-1 rounded-full font-medium border ${
+              className={`px-5 py-1 rounded-full font-medium  ${
                 country === "Germany"
                   ? "bg-black text-white"
-                  : "bg-white text-black border-gray-300"
+                  : " text-black "
               }`}
             >
               Germany
